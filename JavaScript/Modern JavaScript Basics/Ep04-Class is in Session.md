235cf343e17ca5e3b9736a0134059d68e7c939dc
yxj0312 <yanxiajun@hotmail.com>
2022-06-26 22:49:00 +0200

    add notes

---
+++ JavaScript/Modern JavaScript Basics/Ep04-Class is in Session.md
@@ -0,0 +1,11 @@
+# Class is in Session
+
+> If you wanted to use OOP practices in JavaScript before ES6, you'd have to do something pretty hacky with functions and prototypes. Now though, classes are standard in the language and a breeze to work with.
+
+before ES6:
+
+```JavaScript
+
+
+
+```
--- JavaScript/Modern JavaScript Basics/main.js
+++ JavaScript/Modern JavaScript Basics/main.js
@@ -39,57 +39,7 @@
 //     document.getElementById('output').innerHTML = `${username.name}(${username.email})`
 // }
 
-let users = [
-    {
-        name: 'Andrew',
-        email: 'andrew@example.com'
-    },
-    {
-        name: 'Ashley',
-        email: 'ashley@example.com'
-    }
-]
+// 
 
-let names = [];
+// Ep04
 
-// users.forEach(function(user) {
-//     names.push(user.name)
-// })
-
-users.forEach(user => names.push(user.name))
-
-document.getElementById('output').innerHTML = names.join(', ')
-
-// init()
-
-// document.getElementById('btn').addEventListener('click', function() {
-//     function getDetails() {
-//         return `The button id is ${this.getAttribute('id')}`
-//     }
-
-//     document.getElementById('output').innerHTML = getDetails()
-// })
-
-// document.getElementById('btn').addEventListener('click', function() {
-//     const getDetails = () => {
-//         return `The button id is ${this.getAttribute('id')}`
-//     }
-
-//     document.getElementById('output').innerHTML = getDetails()
-// })
-
-// document.getElementById('btn').addEventListener('click', () => {
-//     const getDetails = () => {
-//         return `The button id is ${this.getAttribute('id')}`
-//     }
-
-//     document.getElementById('output').innerHTML = getDetails()
-// })
-
-document.getElementById('btn').addEventListener('click', (e) => {
-    const getDetails = () => {
-        return `The button id is ${e.currentTarget.getAttribute('id')}`
-    }
-
-    document.getElementById('output').innerHTML = getDetails()
-})
