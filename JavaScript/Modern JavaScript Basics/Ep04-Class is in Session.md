b08220567679d36eb00e265a5b9ce8d8730c1fce
yxj0312 <yanxiajun@hotmail.com>
2022-06-30 22:18:52 +0200

    add notes

--- JavaScript/Modern JavaScript Basics/Ep04-Class is in Session.md
+++ JavaScript/Modern JavaScript Basics/Ep04-Class is in Session.md
@@ -44,3 +44,38 @@ document.getElementById('output').innerHTML = purchasedItem.getDetailsWithPrice(
 // Sugar - Food - $2.49
 
 ```
+
+OOP after ES6
+
+```JavaScript
+
+class Item {
+    constructor(name, category) {
+        this.name = name
+        this.category = category
+    } 
+
+    getDetails() {
+        return `${this.name} - ${this.category}`
+    }
+}
+
+class PurchasedItem extends Item {
+    constructor(name, category, price) {
+        super(name, category) 
+        this.price = price
+    }
+
+    getDetailsWithPrice() {
+        return `${this.name} - ${this.category} - $${this.price}`
+    }
+}
+
+let item = new Item('Coffee', 'Food')
+item.category = 'Drinks'
+
+let purchasedItem = new PurchasedItem('Sugar', 'Food', '2.49')
+
+document.getElementById('output').innerHTML = purchasedItem.getDetailsWithPrice()
+
+```
--- JavaScript/Modern JavaScript Basics/main.js
+++ JavaScript/Modern JavaScript Basics/main.js
@@ -76,3 +76,32 @@
 // var purchasedItem = new PurchasedItem('Sugar', 'Food', '2.49')
 
 // document.getElementById('output').innerHTML = purchasedItem.getDetailsWithPrice()
+
+class Item {
+    constructor(name, category) {
+        this.name = name
+        this.category = category
+    } 
+
+    getDetails() {
+        return `${this.name} - ${this.category}`
+    }
+}
+
+class PurchasedItem extends Item {
+    constructor(name, category, price) {
+        super(name, category) 
+        this.price = price
+    }
+
+    getDetailsWithPrice() {
+        return `${this.name} - ${this.category} - $${this.price}`
+    }
+}
+
+let item = new Item('Coffee', 'Food')
+item.category = 'Drinks'
+
+let purchasedItem = new PurchasedItem('Sugar', 'Food', '2.49')
+
+document.getElementById('output').innerHTML = purchasedItem.getDetailsWithPrice()
